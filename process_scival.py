d1c8cd097260792c9505e5eccfae089f240eea9a
Katie Knight <knight.kathryn@gmail.com>
2018-09-06 16:32:27 -0400

    fixed date for file name

--- process_scival.py
+++ process_scival.py
@@ -7,13 +7,12 @@ import prelim, get_files
 
 def main():
 
-######
 	area = prelim.ask_user()
 	df = get_files.getFiles(area)
 
 	out_path = '#outputPath' + '/' + area + '/Processed Data/'
 	
-	data_year = "fix this"
+	data_year = np.unique(df[['Year range']].dropna().as_matrix())[0]
 	
 	writer = pd.ExcelWriter(out_path + area + '_' + data_year + '.xlsx', engine = 'xlsxwriter')
 	df.to_excel(writer, index = False, encoding ='ISO-8859-1')
